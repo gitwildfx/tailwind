a4e0aba35bcfa48831afb20d0481543acd51a36d
WildFx.io <ask@wildfx.io>
2024-12-11 15:05:16 -0500

    Update AuthorLayout.tsx

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 export default function AuthorLayout({ children, content }: Props) {
-  const { name, avatar, occupation, company, email, twitter, bluesky, linkedin, github } = content
+  const { name, avatar, occupation, company, email, twitter, bluesky, linkedin, github, discord } = content
 
   return (
     <>
