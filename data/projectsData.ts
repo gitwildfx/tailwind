d65f663a6d4e61ce5107bf9f5ae39d714532aa5a
WildFx.io <ask@wildfx.io>
2024-12-12 06:52:28 -0500

    Update projectsData.ts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -16,7 +16,7 @@ const projectsData: Project[] = [
     title: 'W001: Introduction to Modern Computing',
     description: `Ever wanted to learn about code but found it too difficult? This 8-week course will take you through the fundamentals of coding languages, like Python, JavaScript, and CSS to help you get coding fast.`,
     imgSrc: '/static/images/code.jpg',
-    href: '/blog/launching-a-dream-startup',
+    href: '#',
   },
 ]
 
