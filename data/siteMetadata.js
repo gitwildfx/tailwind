6da46d86727b1d76f5252cdc52be66d0fe36b653
WildFx.io <ask@wildfx.io>
2024-12-12 10:51:31 -0500

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'How I became a Microsoft Developer, and so can you' + '.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://tailwind-eight-wine.vercel.app',
+  siteRepo: 'https://github.com/wildfxcode/tailwind',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   email: 'ask@wildfx.io',
