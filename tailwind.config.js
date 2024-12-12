a558d6db8adeb751c5f4c81073fe161100c7d995
WildFx.io <ask@wildfx.io>
2024-12-12 10:57:37 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
         sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
       },
       colors: {
-        primary: colors.yellow,
+        primary: colors.FireBrick,
         gray: colors.gray,
       },
       zIndex: {
