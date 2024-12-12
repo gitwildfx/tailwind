fe8c992e430a50004dabba4b342a6f566a674383
WildFx.io <ask@wildfx.io>
2024-12-12 11:03:45 -0500

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -1,5 +1,6 @@
 import { Analytics } from '@vercel/analytics/next';
- 
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 export default function RootLayout({
   children,
 }: {
@@ -13,6 +14,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
