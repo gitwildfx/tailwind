7d5db9f4f3b8788117880625ae9d2b258754c091
WildFx.io <ask@wildfx.io>
2024-12-12 09:48:26 -0500

    Update projectsData.ts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,10 +1,8 @@
-import React from 'react'
-
 interface Project {
-  title: string
-  description: string
-  href?: string
-  imgSrc?: string
+  title: string;
+  description: string;
+  href?: string;
+  imgSrc?: string;
 }
 
 const projectsData: Project[] = [
@@ -14,26 +12,23 @@ const projectsData: Project[] = [
     imgSrc: '/static/images/code.jpg',
     href: 'https://www.prodigalhouse.net/msr', // external URL
   },
-  // Add more projects as necessary
-]
+];
 
-const ProjectComponent = () => {
-  return (
-    <div>
-      {projectsData.map((project) => (
-        <div key={project.title}>
-          <h2>{project.title}</h2>
-          <p>{project.description}</p>
-          {project.imgSrc && <img src={project.imgSrc} alt={project.title} />}
-          {project.href && (
-            <a href={project.href} target="_blank" rel="noopener noreferrer">
-              Visit Project
-            </a>
-          )}
-        </div>
-      ))}
-    </div>
-  )
-}
+const ProjectComponent = () => (
+  <div>
+    {projectsData.map((project) => (
+      <div key={project.title}>
+        <h2>{project.title}</h2>
+        <p>{project.description}</p>
+        {project.imgSrc && <img src={project.imgSrc} alt={project.title} />}
+        {project.href && (
+          <a href={project.href} target="_blank" rel="noopener noreferrer">
+            Visit Project
+          </a>
+        )}
+      </div>
+    ))}
+  </div>
+);
 
-export default ProjectComponent
+export default ProjectComponent;
