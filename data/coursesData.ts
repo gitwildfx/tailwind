97aa4f6ab7c4318bea78c37c499dda68f819b3ee
WildFx.io <ask@wildfx.io>
2024-12-13 08:53:29 -0500

    Update coursesData.ts

--- data/coursesData.ts
+++ data/coursesData.ts
@@ -1,9 +1,17 @@
+// Define the Project type
+type Course = {
+  title: string;
+  description: string;
+  imgSrc: string;
+  href: string;
+};
+
 // app/data/coursesData.ts
 const coursesData = [
   {
     title: 'W001: Intro to Modern Computing (Spring 2025)',
     description: 'An introduction to programming languages for people of all skill levels and backgrounds. We will be exploring Javascript, Python, HTML among other tools like AI/LLMs needed as a developer.',
-    imgSrc: '/images/code.jpg',
+    imgSrc: '/static/images/code.jpg',
     href: '#',
   },
 ];
