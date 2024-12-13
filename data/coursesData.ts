fe8b11b6bb9ee9a007b47706d863a90e292f1715
WildFx.io <ask@wildfx.io>
2024-12-13 08:30:33 -0500

    Update coursesData.ts

--- data/coursesData.ts
+++ data/coursesData.ts
@@ -1,22 +1,10 @@
 // app/data/coursesData.ts
 const coursesData = [
   {
-    title: 'Course 1',
-    description: 'An introduction to programming.',
-    imgSrc: '/images/course1.jpg',
-    href: '/courses/course1',
-  },
-  {
-    title: 'Course 2',
-    description: 'Mastering web development.',
-    imgSrc: '/images/course2.jpg',
-    href: '/courses/course2',
-  },
-  {
-    title: 'Course 3',
-    description: 'Advanced JavaScript techniques.',
-    imgSrc: '/images/course3.jpg',
-    href: '/courses/course3',
+    title: 'W001: Intro to Modern Computing (Spring 2025)',
+    description: 'An introduction to programming languages for people of all skill levels and backgrounds. We will be exploring Javascript, Python, HTML among other tools like AI/LLMs needed as a developer. 
+    imgSrc: '/images/code.jpg',
+    href: '#',
   },
 ];
 
