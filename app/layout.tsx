0726ec8131da958c0756b21dc1716724508ccf30
WildFx.io <ask@wildfx.io>
2024-12-13 07:36:24 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,65 +1,71 @@
-import 'css/tailwind.css'
-import 'pliny/search/algolia.css'
-import 'remark-github-blockquote-alert/alert.css'
+import { Analytics } from '@vercel/analytics/next';
 
-import { Space_Grotesk } from 'next/font/google'
-import { Analytics, AnalyticsConfig } from 'pliny/analytics'
-import { SearchProvider, SearchConfig } from 'pliny/search'
-import Header from '@/components/Header'
-import SectionContainer from '@/components/SectionContainer'
-import Footer from '@/components/Footer'
-import siteMetadata from '@/data/siteMetadata'
-import { ThemeProviders } from './theme-providers'
-import { Metadata } from 'next'
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  import 'css/tailwind.css';
+  import 'pliny/search/algolia.css';
+  import 'remark-github-blockquote-alert/alert.css';
 
-const space_grotesk = Space_Grotesk({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-space-grotesk',
-})
+  import { Space_Grotesk } from 'next/font/google';
+  import { Analytics, AnalyticsConfig } from 'pliny/analytics';
+  import { SearchProvider, SearchConfig } from 'pliny/search';
+  import Header from '@/components/Header';
+  import SectionContainer from '@/components/SectionContainer';
+  import Footer from '@/components/Footer';
+  import siteMetadata from '@/data/siteMetadata';
+  import { ThemeProviders } from './theme-providers';
+  import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  metadataBase: new URL(siteMetadata.siteUrl),
-  title: {
-    default: siteMetadata.title,
-    template: `%s | ${siteMetadata.title}`,
-  },
-  description: siteMetadata.description,
-  openGraph: {
-    title: siteMetadata.title,
-    description: siteMetadata.description,
-    url: './',
-    siteName: siteMetadata.title,
-    images: [siteMetadata.socialBanner],
-    locale: 'en_US',
-    type: 'website',
-  },
-  alternates: {
-    canonical: './',
-    types: {
-      'application/rss+xml': `${siteMetadata.siteUrl}/feed.xml`,
+  const space_grotesk = Space_Grotesk({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-space-grotesk',
+  });
+
+  export const metadata: Metadata = {
+    metadataBase: new URL(siteMetadata.siteUrl),
+    title: {
+      default: siteMetadata.title,
+      template: `%s | ${siteMetadata.title}`,
     },
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
+    description: siteMetadata.description,
+    openGraph: {
+      title: siteMetadata.title,
+      description: siteMetadata.description,
+      url: './',
+      siteName: siteMetadata.title,
+      images: [siteMetadata.socialBanner],
+      locale: 'en_US',
+      type: 'website',
+    },
+    alternates: {
+      canonical: './',
+      types: {
+        'application/rss+xml': `${siteMetadata.siteUrl}/feed.xml`,
+      },
+    },
+    robots: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      googleBot: {
+        index: true,
+        follow: true,
+        'max-video-preview': -1,
+        'max-image-preview': 'large',
+        'max-snippet': -1,
+      },
     },
-  },
-  twitter: {
-    title: siteMetadata.title,
-    card: 'summary_large_image',
-    images: [siteMetadata.socialBanner],
-  },
-}
+    twitter: {
+      title: siteMetadata.title,
+      card: 'summary_large_image',
+      images: [siteMetadata.socialBanner],
+    },
+  };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const basePath = process.env.BASE_PATH || ''
+  const basePath = process.env.BASE_PATH || '';
 
   return (
     <html
@@ -107,5 +113,5 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </ThemeProviders>
       </body>
     </html>
-  )
+  );
 }
