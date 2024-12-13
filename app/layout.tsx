198d477e10ac3f3653016632dedbc22ca27ed229
WildFx.io <ask@wildfx.io>
2024-12-13 07:43:59 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,72 +1,71 @@
 import { Analytics } from '@vercel/analytics/next';
+import 'css/tailwind.css';
+import 'pliny/search/algolia.css';
+import 'remark-github-blockquote-alert/alert.css';
+
+import { Space_Grotesk } from 'next/font/google';
+import { Analytics as PlinyAnalytics, AnalyticsConfig } from 'pliny/analytics';
+import { SearchProvider, SearchConfig } from 'pliny/search';
+import Header from '@/components/Header';
+import SectionContainer from '@/components/SectionContainer';
+import Footer from '@/components/Footer';
+import siteMetadata from '@/data/siteMetadata';
+import { ThemeProviders } from './theme-providers';
+import { Metadata } from 'next';
+
+const space_grotesk = Space_Grotesk({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-space-grotesk',
+});
+
+export const metadata: Metadata = {
+  metadataBase: new URL(siteMetadata.siteUrl),
+  title: {
+    default: siteMetadata.title,
+    template: `%s | ${siteMetadata.title}`,
+  },
+  description: siteMetadata.description,
+  openGraph: {
+    title: siteMetadata.title,
+    description: siteMetadata.description,
+    url: './',
+    siteName: siteMetadata.title,
+    images: [siteMetadata.socialBanner],
+    locale: 'en_US',
+    type: 'website',
+  },
+  alternates: {
+    canonical: './',
+    types: {
+      'application/rss+xml': `${siteMetadata.siteUrl}/feed.xml`,
+    },
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  twitter: {
+    title: siteMetadata.title,
+    card: 'summary_large_image',
+    images: [siteMetadata.socialBanner],
+  },
+};
+
+const basePath = process.env.BASE_PATH || '';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  import 'css/tailwind.css';
-  import 'pliny/search/algolia.css';
-  import 'remark-github-blockquote-alert/alert.css';
-
-  import { Space_Grotesk } from 'next/font/google';
-  import { Analytics, AnalyticsConfig } from 'pliny/analytics';
-  import { SearchProvider, SearchConfig } from 'pliny/search';
-  import Header from '@/components/Header';
-  import SectionContainer from '@/components/SectionContainer';
-  import Footer from '@/components/Footer';
-  import siteMetadata from '@/data/siteMetadata';
-  import { ThemeProviders } from './theme-providers';
-  import { Metadata } from 'next';
-
-  const space_grotesk = Space_Grotesk({
-    subsets: ['latin'],
-    display: 'swap',
-    variable: '--font-space-grotesk',
-  });
-
-  export const metadata: Metadata = {
-    metadataBase: new URL(siteMetadata.siteUrl),
-    title: {
-      default: siteMetadata.title,
-      template: `%s | ${siteMetadata.title}`,
-    },
-    description: siteMetadata.description,
-    openGraph: {
-      title: siteMetadata.title,
-      description: siteMetadata.description,
-      url: './',
-      siteName: siteMetadata.title,
-      images: [siteMetadata.socialBanner],
-      locale: 'en_US',
-      type: 'website',
-    },
-    alternates: {
-      canonical: './',
-      types: {
-        'application/rss+xml': `${siteMetadata.siteUrl}/feed.xml`,
-      },
-    },
-    robots: {
-      index: true,
-      follow: true,
-      googleBot: {
-        index: true,
-        follow: true,
-        'max-video-preview': -1,
-        'max-image-preview': 'large',
-        'max-snippet': -1,
-      },
-    },
-    twitter: {
-      title: siteMetadata.title,
-      card: 'summary_large_image',
-      images: [siteMetadata.socialBanner],
-    },
-  };
-
-  const basePath = process.env.BASE_PATH || '';
-
   return (
     <html
       lang={siteMetadata.language}
@@ -102,7 +101,7 @@ export default function RootLayout({
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
-          <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
+          <PlinyAnalytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
             <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
               <Header />
