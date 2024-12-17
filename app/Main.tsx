b128a69407568b3a4f26695231fa135d39c04368
WildFx.io <ask@wildfx.io>
2024-12-17 17:53:48 -0500

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -11,6 +11,7 @@ export default function Home({ posts }) {
     <>
       {/* Title and Description */}
       <div className="my-4 flex flex-col items-center gap-x-12 xl:mb-12 xl:flex-row">
+        {/* Title and Description */}
         <div className="flex-1 pt-4">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             &lt;/Learn_Code&gt;
