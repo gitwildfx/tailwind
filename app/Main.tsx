e5b3abcd574f987cf2fd820318f6e51965daeb63
WildFx.io <ask@wildfx.io>
2024-12-18 17:08:25 -0500

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -17,10 +17,10 @@ export default function Home({ posts }) {
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             &lt;/Learn_Code&gt;
           </h1>
-          <p className="mt-4 text-lg leading-7 text-gray-500 dark:text-gray-400 mb-6">
+          <p className="mt-4 text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
           </p>
-          <h2 className="center text-lg text-gray-600 dark:text-gray-400 mt-6">
+          <h2 className="center text-lg text-gray-600 dark:text-gray-400">
             {`If you're curious about learning code, or want to grow in the tech space, follow the journey. `}
             <a
               href="https://wildfx.io/blog/launching-a-dream-startup"
