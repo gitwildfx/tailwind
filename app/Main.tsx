343cccdfa9d73f7ae50d36f0b0ea913844eebfff
WildFx.io <ask@wildfx.io>
2024-12-18 14:17:24 -0500

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -10,91 +10,82 @@ export default function Home({ posts }) {
   return (
     <>
       {/* Title and Description */}
-      <div className="my-6 flex flex-col items-center justify-center gap-x-12 xl:mb-12 xl:flex-row">
-        {/* Title Section */}
-        <div className="mr-8 pt-6">
-          <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+      <div className="my-3 flex flex-col items-center gap-x-12 xl:mb-9 xl:flex-row">
+        {/* In-between margin */}
+        <div className="flex-1 pt-3">
+          {/* In-between padding top */}
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             &lt;/Learn_Code&gt;
           </h1>
-          <h2 className="prose text-center text-lg text-gray-600 dark:text-gray-400">
-            {`How I became a Developer, and you can too! If you're curious about learning code, or want to grow in the tech space, follow the journey. `}
-            <a
-              href="https://wildfx.io/blog/launching-a-dream-startup"
-              className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            >
-              Learn
-            </a>
-            {` how we're changing the way people learn and interact with code for the modern age.`}
-          </h2>
+          <p className="mt-4 text-lg leading-7 text-gray-500 dark:text-gray-400">
+            {siteMetadata.description}
+          </p>
         </div>
 
-        {/* Newsletter Form Section */}
-        <div className="my-12 mx-2 flex w-[300px] items-center justify-center sm:w-[400px] md:w-[550px]">
+        {/* Newsletter Form */}
+        <div className="mx-2 my-12 flex w-[300px] items-center justify-center sm:w-[400px] md:w-[550px]">
           <NewsletterForm title="Subscribe to the newsletter" />
         </div>
       </div>
 
       {/* Latest Posts */}
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h2 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-12">
             Articles
           </h2>
         </div>
 
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-          {!posts.length ? (
-            <p>No posts found.</p>
-          ) : (
-            posts.slice(0, MAX_DISPLAY).map((post) => {
-              const { slug, date, title, summary, tags } = post
-              return (
-                <li key={slug} className="py-12">
-                  <article>
-                    <div className="xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0 space-y-2">
-                      <dl>
-                        <dt className="sr-only">Published on</dt>
-                        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                          <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
-                        </dd>
-                      </dl>
-                      <div className="xl:col-span-3 space-y-5">
-                        <div className="space-y-6">
-                          <div>
-                            <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                              <Link
-                                href={`/blog/${slug}`}
-                                className="text-gray-900 dark:text-gray-100"
-                              >
-                                {title}
-                              </Link>
-                            </h3>
-                            <div className="flex flex-wrap">
-                              {tags.map((tag) => (
-                                <Tag key={tag} text={tag} />
-                              ))}
-                            </div>
-                          </div>
-                          <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                            {summary}
+          {!posts.length && 'No posts found.'}
+          {posts.slice(0, MAX_DISPLAY).map((post) => {
+            const { slug, date, title, summary, tags } = post
+            return (
+              <li key={slug} className="py-12">
+                <article>
+                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                    <dl>
+                      <dt className="sr-only">Published on</dt>
+                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                        <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+                      </dd>
+                    </dl>
+                    <div className="space-y-5 xl:col-span-3">
+                      <div className="space-y-6">
+                        <div>
+                          <h3 className="text-2xl font-bold leading-8 tracking-tight">
+                            <Link
+                              href={`/blog/${slug}`}
+                              className="text-gray-900 dark:text-gray-100"
+                            >
+                              {title}
+                            </Link>
+                          </h3>
+                          <div className="flex flex-wrap">
+                            {tags.map((tag) => (
+                              <Tag key={tag} text={tag} />
+                            ))}
                           </div>
                         </div>
-                        <div className="text-base font-medium leading-6">
-                          <Link
-                            href={`/blog/${slug}`}
-                            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                            aria-label={`Read "${title}"`}
-                          >
-                            Read more &rarr;
-                          </Link>
+                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                          {summary}
                         </div>
                       </div>
+                      <div className="text-base font-medium leading-6">
+                        <Link
+                          href={`/blog/${slug}`}
+                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          aria-label={`Read "${title}"`}
+                        >
+                          Read more &rarr;
+                        </Link>
+                      </div>
                     </div>
-                  </article>
-                </li>
-              )
-            })
-          )}
+                  </div>
+                </article>
+              </li>
+            )
+          })}
         </ul>
       </div>
 
