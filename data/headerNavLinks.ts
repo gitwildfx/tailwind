2ef1d4ac9b95cfd3cb40301493008161378397a3
WildFx.io <ask@wildfx.io>
2024-12-20 09:19:43 -0500

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
   { href: '/courses', title: 'Courses' },
+  { href: '/store', title: 'Store' },
   { href: '/about', title: 'About' },
 ]
 
