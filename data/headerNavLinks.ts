68c98a30f8279f49aea1d0cbd68a9cc11b1b51ac
WildFx.io <ask@wildfx.io>
2024-12-20 11:28:04 -0500

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,7 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
   { href: '/courses', title: 'Courses' },
-  { href: '/store', title: 'Store' },
+  { href: 'https://www.bonfire.com/store/wildfx/', title: 'Store' },
   { href: '/about', title: 'About' },
 ]
 
