28885732ef85e0f86838cfad2089c7b286beea8e
WildFx.io <ask@wildfx.io>
2024-12-20 17:14:32 -0500

    Update page.tsx

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -13,7 +13,7 @@ export default function Courses() {
             Courses
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Explore our current course offerings and learning resources:
+            Explore course offerings and learning resources:
           </p>
         </div>
         <div className="container py-12">
