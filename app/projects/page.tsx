498f4bcfbfdca9713bdfc11d7ff40ded5878941c
WildFx.io <ask@wildfx.io>
2024-12-20 17:14:15 -0500

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Review our code projects, tools, and publications:
+            Review code projects, tools, and publications:
           </p>
         </div>
         <div className="container py-12">
