6e1f058eef97aeda6370b3f77853d0db350318d6
WildFx.io <ask@wildfx.io>
2024-12-29 17:45:04 -0500

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,24 +3,35 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
+import Image from 'next/image'
 
 const MAX_DISPLAY = 5
 
 export default function Home({ posts }) {
   return (
     <>
-      {/* Title and Description */}
-      <div className="my-3 flex flex-col items-center gap-x-12 xl:mb-9 xl:flex-row">
-        {/* In-between margin */}
+      <div className="my-3 flex flex-col items-center gap-x-12 xl:mb-9 xl:flex-row xl:justify-between">
         <div className="flex-1 pt-3">
-          {/* In-between padding top */}
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             &lt;/Learn_Code&gt;
           </h1>
           <p className="mb-6 mt-4 text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
           </p>
-          <h2 className="center text-lg text-gray-600 dark:text-gray-400">
+          <div className="relative mx-0 my-4 flex w-full justify-center xl:w-full xl:justify-start">
+            <div className="mb-2 w-full max-w-[600px]">
+              <a href="https://github.com/gitwildfx" target="_blank">
+                <Image
+                  src="https://raw.githubusercontent.com/gitwildfx/tailwind/main/public/static/images/timlrx-github-contributions.svg"
+                  alt="WildFx GitHub Contributions Chart"
+                  className="h-auto max-w-full object-contain"
+                  width={600}
+                  height={400}
+                />
+              </a>
+            </div>
+          </div>
+          <h2 className="center mb-4 text-lg text-gray-600 dark:text-gray-400">
             {`Interested in a new career transition? Check out our available `}
             <a
               href="https://wildfx.io/courses"
@@ -38,21 +49,16 @@ export default function Home({ posts }) {
             {`. Discover how we're changing the way people learn and interact with data in the modern age.`}
           </h2>
         </div>
-
-        {/* Newsletter Form */}
-        <div className="mx-2 my-12 flex w-[300px] items-center justify-center sm:w-[400px] md:w-[550px]">
+        <div className="mb-12 mt-4 flex w-full max-w-[600px] justify-center xl:w-1/2 xl:justify-start">
           <NewsletterForm title="Subscribe to the newsletter" />
         </div>
       </div>
-
-      {/* Latest Posts */}
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h2 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-12">
             Articles
           </h2>
         </div>
-
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
@@ -105,8 +111,6 @@ export default function Home({ posts }) {
           })}
         </ul>
       </div>
-
-      {/* View All Posts */}
       {posts.length > MAX_DISPLAY && (
         <div className="flex justify-end text-base font-medium leading-6">
           <Link
