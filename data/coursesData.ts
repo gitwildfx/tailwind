3b9263d30efbe24b80608210324ed8caec5cf70e
WildFx.io <ask@wildfx.io>
2024-12-31 17:43:11 -0500

    Update coursesData.ts

--- data/coursesData.ts
+++ data/coursesData.ts
@@ -12,7 +12,7 @@ const coursesData = [
     title: 'W001: Intro to Modern Computing (Spring 2025)',
     description: 'An introduction to programming languages for people of all skill levels. We will be exploring Javascript, Python, and HTML among other helpful tools like AI/LLMs for code development.',
     imgSrc: '/static/images/code.jpg',
-    href: '#',
+    href: '/blog/intro-to-modern-computing',
   },
 ];
 
