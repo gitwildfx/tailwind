f4d19375508ee3d42e1c8f440fed46b93c61040e
WildFx.io <ask@wildfx.io>
2025-01-01 11:10:03 -0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,9 +6,8 @@ import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 
 const Header = () => {
-  let headerClass =
-    'flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10'
-  if (siteMetadata.stickyNav) {
+  let headerClass = 'flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10'
+    if (siteMetadata.stickyNav) {
     headerClass += ' sticky top-0 z-50'
   }
 
