d452dc84f4bf9f76d7dbefdb6f00467198f47634
WildFx.io <ask@wildfx.io>
2025-01-01 10:49:33 -0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,6 @@ import Logo from '@/data/logo.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
-import SearchButton from './SearchButton'
 
 const Header = () => {
   let headerClass = 'flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10'
@@ -31,18 +30,21 @@ const Header = () => {
       <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
         <div className="no-scrollbar hidden max-w-40 items-center space-x-4 overflow-x-auto sm:flex sm:space-x-6 md:max-w-72 lg:max-w-96">
           {headerNavLinks
-            .filter((link) => link.href !== '/')
+            .filter((link) => link.href !== '/') // Ensure the home page link is excluded
             .map((link) => (
               <Link
                 key={link.title}
                 href={link.href}
-                className="block font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
+                className={
+                  link.isButton
+                    ? 'rounded border border-orange-500 bg-transparent px-4 py-2 font-semibold text-orange-500 hover:border-transparent hover:bg-orange-500 hover:text-white'
+                    : 'block font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400'
+                }
               >
                 {link.title}
               </Link>
             ))}
         </div>
-        <SearchButton />
         <ThemeSwitch />
         <MobileNav />
       </div>
