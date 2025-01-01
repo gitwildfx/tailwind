fa5f155a38a684beb7c5c45718ce0acc4487eb05
WildFx.io <ask@wildfx.io>
2024-12-31 20:09:48 -0500

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -49,8 +49,7 @@ export default function Home({ posts }) {
             {`. Discover how we're changing the way people learn and interact with data in the modern age.`}
           </h2>
         </div>
-        <div id="newsletterSection">
-        </div>
+        <div id="newsletterSection" />
         <div className="mb-12 mt-4 flex w-full max-w-[600px] justify-center xl:w-1/2 xl:justify-start">
           <NewsletterForm title="Subscribe to the newsletter" />
         </div>
