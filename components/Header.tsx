9c0fb1a45cc14d1d391ccd2edfd625d5f35064ca
WildFx.io <ask@wildfx.io>
2025-01-01 11:47:40 -0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
           )}
         </div>
       </Link>
-      <div className="flex items-center space-x-4 leading-5 sm:space-x-6 w-full justify-between">
+      <div className="flex items-center w-full justify-between space-x-4 leading-5 sm:space-x-6">
         {/* Desktop and larger screens: Navigation links */}
         <div className="no-scrollbar hidden w-auto items-center space-x-4 overflow-x-auto sm:flex sm:space-x-6 md:max-w-72 lg:max-w-96">
           {headerNavLinks
@@ -48,7 +48,7 @@ const Header = () => {
         </div>
 
         {/* Mobile only: Toggle button and mobile menu */}
-        <div className="sm:hidden flex items-center justify-between w-full">
+        <div className="flex w-full items-center justify-between sm:hidden">
           <MobileNav />
         </div>
 
