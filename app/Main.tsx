3efe559bb7eeaca07e8bd8c2f8e8aa347851bf09
WildFx.io <ask@wildfx.io>
2025-01-02 08:30:34 -0500

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -70,7 +70,7 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h2 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-12">
-            Articles
+            Updates
           </h2>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
