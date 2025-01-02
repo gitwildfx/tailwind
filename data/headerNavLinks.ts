aa207c016e7bbe0fa3b30694f4da64b616db8346
WildFx.io <ask@wildfx.io>
2025-01-02 08:30:39 -0500

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/blog/code-of-the-lion', title: 'Start>' },
-  { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Learn' },
   { href: '/courses', title: 'Courses' },
+  { href: '/blog', title: 'Updates' },
   { href: 'https://www.bonfire.com/store/wildfx/', title: 'Store' },
   { href: '/about', title: 'About' },
 ]
