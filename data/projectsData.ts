03f35ea63cb74467ed751ad713f1167238b402b5
WildFx.io <ask@wildfx.io>
2025-01-08 15:54:17 -0500

    Update projectsData.ts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -11,7 +11,7 @@ const projectsData: Project[] = [
   {
     title: 'Docusaurus',
     description: `Build a customized Document repository from the ground up. Employ a live Code Enviornment to design, deploy, and launch your very first website. Plus, you'll learn about dinosaurs...not really.`,
-    imgSrc: '/static/images/docusaurus_live.gif',
+    imgSrc: '/static/images/docusaurus-1.png',
     href: 'https://docusaurus-fqlz.vercel.app/', // relative URL
   },
   {
