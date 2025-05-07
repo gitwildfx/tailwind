c212c32a8ce5718a9b7036a7bb0b9b7e74adf4d8
WildFx.io <mail.wildfx@gmail.com>
2025-05-07 11:36:34 -0400

    Update coursesData.ts

--- data/coursesData.ts
+++ data/coursesData.ts
@@ -9,7 +9,7 @@ type Course = {
 // app/data/coursesData.ts
 const coursesData = [
   {
-    title: 'W0001: Intro to Modern Computing (June 11-12 @ 7-9PM EST)',
+    title: 'W001: Intro to Modern Computing',
     description: 'An introduction to programming languages for people of all skill levels. We will be exploring Javascript, Python, and HTML among other helpful tools like AI/LLMs for code development.',
     imgSrc: '/static/images/code.jpg',
     href: '/blog/intro-to-modern-computing',
