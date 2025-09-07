84e878d1e809f6853b0864dc8e9f7b2cbb4e5078
WildFx.io <mail.wildfx@gmail.com>
2025-09-06 20:07:23 -0400

    Update MDXComponents.tsx

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,16 +1,40 @@
+// src/components/MDXComponents.tsx
+
+import type { MDXComponents } from 'mdx/types'
+
+// Pliny UI components
 import TOCInline from 'pliny/ui/TOCInline'
 import Pre from 'pliny/ui/Pre'
 import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
-import type { MDXComponents } from 'mdx/types'
+
+// Local custom components
 import Image from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
+import AudioPlayer from '@/components/HomepageFeatures/AudioPlayer'
 
+/**
+ * MDXComponents provides custom React components
+ * that can be used directly inside .mdx files.
+ *
+ * Example usage in MDX:
+ * 
+ * ```mdx
+ * <AudioPlayer />
+ * <TOCInline toc={toc} />
+ * ```
+ */
 export const components: MDXComponents = {
+  // Default overrides
   Image,
-  TOCInline,
   a: CustomLink,
   pre: Pre,
   table: TableWrapper,
+
+  // Pliny extras
+  TOCInline,
   BlogNewsletterForm,
+
+  // Custom site components
+  AudioPlayer,
 }
