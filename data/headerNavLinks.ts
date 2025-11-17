b517bc3d0dfd57ce1d598cf182c99caef3036eee
WildFx.io <mail.wildfx@gmail.com>
2025-11-16 23:15:49 -0500

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,6 @@ const headerNavLinks = [
   { href: '/projects', title: 'Learn>' },
   { href: '/blog', title: 'Blog' },
   { href: '/courses', title: 'Courses' },
-  { href: 'https://www.bonfire.com/store/wildfx/', title: 'Store' },
   { href: '/about', title: 'About' },
 ]
 
